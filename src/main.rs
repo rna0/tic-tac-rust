dcdcd0a0e6562051f20bb7be07fc127d8368b170
RNA <roy.arama1@gmail.com>
2022-03-21 22:49:32 +0200

    new array and scroll in gui

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,40 @@
+use eframe::egui::{CentralPanel, Context, ScrollArea};
+use eframe::epaint::Vec2;
 use eframe::epi::{App, Frame};
-use eframe::{NativeOptions, run_native};
-use eframe::egui::{CentralPanel, Context};
+use eframe::{run_native, NativeOptions};
 
 struct TicTacToeCells {
-    cells: Vec<TicTacToeCell>
-}
-
-struct TicTacToeCell {
-    cell: String
+    cells: Vec<TicTacToeCell>,
 }
 
 impl TicTacToeCells {
     fn new() -> TicTacToeCells {
-        let iter: impl Iterator<Item=TicTacToeCell> = (0..9).map(|a| TicTacToeCell {
-            cell: format!("X{}", a)
+        let iter = (0..90).map(|a| TicTacToeCell {
+            cell: format!("X{}", a),
         });
-        TicTacToeCells{
-            cells: Vec::from_iter(iter)
+        TicTacToeCells {
+            cells: Vec::from_iter(iter),
         }
     }
 }
 
+struct TicTacToeCell {
+    cell: String,
+}
+
 impl App for TicTacToeCells {
+    fn setup(&mut self, _ctx: &eframe::egui::Context, _frame: &Frame, _storage: Option<&dyn eframe::epi::Storage>) {
+        
+    }
+
     fn update(&mut self, ctx: &Context, frame: &Frame) {
-        CentralPanel::default().show(ctx, |ui| {});
+        CentralPanel::default().show(ctx, |ui| {
+            ScrollArea::vertical().show(ui, |ui| {
+                for a in &self.cells {
+                    ui.label(&a.cell);
+                }
+            })
+        });
     }
 
     fn name(&self) -> &str {
@@ -32,7 +43,8 @@ impl App for TicTacToeCells {
 }
 
 fn main() {
-    let app = TicTacToeCells:new();
-    let win_option = NativeOptions::default();
+    let app = TicTacToeCells::new();
+    let mut win_option = NativeOptions::default();
+    win_option.initial_window_size = Some(Vec2::new(540., 480.));
     run_native(Box::new(app), win_option);
 }
