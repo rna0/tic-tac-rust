5dd6256162aab2fadda397229aef063074b55de8
9itamar9 <9itamar9@gmail.com>
2022-04-02 18:42:02 +0300

    Now in rows

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use eframe::egui::{Button, CentralPanel, Context, ScrollArea};
+use eframe::egui::{self, CentralPanel, Context, CtxRef, Response, Ui};
 use eframe::epaint::Vec2;
 use eframe::epi::{App, Frame};
 use eframe::{run_native, NativeOptions};
@@ -23,17 +23,19 @@ struct TicTacToeCell {
 }
 
 impl App for TicTacToeCells {
-    fn setup(&mut self, _ctx: &eframe::egui::Context, _frame: &Frame, _storage: Option<&dyn eframe::epi::Storage>) {
-        
+    fn setup(&mut self, ctx: &CtxRef, _frame: &Frame, _storage: Option<&dyn eframe::epi::Storage>) {
+        self.configure_fonts(ctx);
     }
 
     fn update(&mut self, ctx: &Context, frame: &Frame) {
         CentralPanel::default().show(ctx, |ui| {
-            for a in &self.cells {
-                
-                if ui.button(&a.cell).clicked() {
-                    
-                }
+            for row in self.cells.chunks(3) {
+                ui.horizontal(|ui| {
+                    for cell in row {
+                        let coolButton = create_cool_button(ui, cell);
+                        if coolButton.clicked() {}
+                    }
+                });
             }
         });
     }
@@ -41,6 +43,34 @@ impl App for TicTacToeCells {
     fn name(&self) -> &str {
         "TicTacToe"
     }
+}
+
+impl TicTacToeCells {
+    pub fn configure_fonts(&mut self, ctx: &CtxRef) {
+        let mut font_def = FontDefinitions::default();
+        font_def.font_data.insert(
+            "MesloLGS".to_string(),
+            Cow::Borrowed(include_bytes!("../../MesloLGS_NF_Regular.ttf")),
+        );
+        font_def.family_and_size.insert(
+            eframe::egui::TextStyle::Heading,
+            (FontFamily::Proportional, 35.),
+        );
+        font_def.family_and_size.insert(
+            eframe::egui::TextStyle::Body,
+            (FontFamily::Proportional, 20.),
+        );
+        font_def
+            .fonts_for_family
+            .get_mut(&FontFamily::Proportional)
+            .unwrap()
+            .insert(0, "MesloLGS".to_string());
+        ctx.set_fonts(font_def);
+    }
+}
+
+fn create_cool_button(ui: &mut Ui, cell: &TicTacToeCell) -> Response {
+    ui.button(&cell.cell)
 }
 
 fn main() {
